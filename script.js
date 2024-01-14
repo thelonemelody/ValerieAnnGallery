0089bb13c74e1c85d84899bf8f19cab943e45f58
Herrera, Lucas <lherrera40@gatech.edu>
2024-01-13 18:51:44 -0600

    second try with no text file white

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    displayText(); // Call displayText function when the page loads
+});
 function displayText() {
     var numberInput = document.getElementById("numberInput").value;
     var resultDiv = document.getElementById("result");
 
     if (numberInput === "") {
-        resultDiv.textContent = "Please enter a number.";
+        resultDiv.innerHTML = `<div class='response'>
+                               <span class='title'>Less is more</span>
+                               <br>
+                               <span class='body-text'>Why?</span>
+                               </div>`;
     } else {
+        var title, bodyText;
         switch (parseInt(numberInput)) {
             case 1:
-                resultDiv.textContent = "You entered One.";
+                title = "Number One";
+                bodyText = "You entered the number One.";
                 break;
             case 2:
-                resultDiv.textContent = "You entered Two.";
+                title = "Number Two";
+                bodyText = "You entered the number Two.";
                 break;
             case 3:
-                resultDiv.textContent = "You entered Three.";
+                title = "Number Three";
+                bodyText = "You entered the number Three.";
                 break;
             default:
-                resultDiv.textContent = "You entered a number greater than 3.";
+                title = "Greater than Three";
+                bodyText = "You entered a number greater than 3.";
                 break;
         }
+
+        resultDiv.innerHTML = `<div class='response'>
+                               <span class='title'>${title}:</span>
+                               <br>
+                               <span class='body-text'>${bodyText}</span>
+                               </div>`;
     }
 }
